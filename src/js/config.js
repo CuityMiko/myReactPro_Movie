ad0fbb9b47a2e1ab44a1213c0fe14cb7aea72a6e
CuityMiko <15201776956@163.com>
2017-06-17 09:21:19 +0800

    修改ServerURL

--- src/js/config.js
+++ src/js/config.js
@@ -2,8 +2,8 @@
  * 设置全局配置变量
  */
 export default{
-    ServerHostURL:'http://www.cuitymiko.com:3389',
-    //ServerHostURL:'http://192.168.0.104:3389',
+    //ServerHostURL:'http://www.cuitymiko.com:3389',
+    ServerHostURL:'http://192.168.0.104:3389',
     PageSize:12,
     City:'杭州',
     version:'0.0.1'
